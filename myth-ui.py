0026b2a70a84f57b59d3101ad7d14b2025ff4502
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2023-06-21 15:32:09 -0300

    Add files via upload

--- myth-ui.py
+++ myth-ui.py
@@ -10,18 +10,20 @@ def init_ui():
 
     input_frame = [
         [
-            sg.Frame('', [
+            sg.Frame('File assets', [
                 [   
-                    sg.Text('Solidity File: ', size=(15, 1)), 
+                    sg.Text('Solidity File: ', size=(10, 1), font='arial 11 bold'), sg.Stretch(),
                     
                     sg.Combo(
                         values=['value' for _ in range(8)], ## placeholder
                         default_value=None,
-                        size=(15,6)
-                    )            
+                        size=(17,6),
+                        font='arial 11'
+                    ), 
+                    
                 ],
                 [
-                    sg.FolderBrowse('...', font='bold'), sg.Input(key='FOLDER', enable_events=True)
+                    sg.FolderBrowse('..', font='arial 9 bold', size=(1,0)), sg.Stretch(), sg.Input('PLACEHOLDER', key='FOLDER', enable_events=True, size=(28,3))
                 ],
                 [sg.Button('SUBMIT', font='verdana 15 bold', button_color='black on ')]
             ])
@@ -30,8 +32,8 @@ def init_ui():
 
 
     layout = [
+        [sg.Column(input_frame)]
         
-        input_frame
     ]
 
 
