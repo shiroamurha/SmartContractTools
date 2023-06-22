c28f6605b1d47a1730c763e6d94f3326ce3717b2
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2023-06-22 19:45:24 -0300

    Add files via upload

--- myth-ui.py
+++ myth-ui.py
@@ -7,34 +7,55 @@ def init_ui():
     global layout
     #sg.Input(key='solc', size=(15, 2)
     sg.theme('DarkAmber')
+    frames_size = (310, 260)
 
-    input_frame = [
+    input_frame = [[sg.Frame('File Assets', [
+        [   
+            sg.Text('Solidity File: ', size=(10, 1), font='arial 11 bold'), sg.Stretch(), 
+            
+            sg.Combo(
+                values=['value' for _ in range(8)], ## placeholder
+                default_value=None,
+                size=(23,6),
+                font='arial 11',
+                key='sol file'
+            ), sg.Push()
+        ],
+
         [
-            sg.Frame('File assets', [
-                [   
-                    sg.Text('Solidity File: ', size=(10, 1), font='arial 11 bold'), sg.Stretch(),
-                    
-                    sg.Combo(
-                        values=['value' for _ in range(8)], ## placeholder
-                        default_value=None,
-                        size=(17,6),
-                        font='arial 11'
-                    ), 
-                    
-                ],
-                [
-                    sg.FolderBrowse('..', font='arial 9 bold', size=(1,0)), sg.Stretch(), sg.Input('PLACEHOLDER', key='FOLDER', enable_events=True, size=(28,3))
-                ],
-                [sg.Button('SUBMIT', font='verdana 15 bold', button_color='black on ')]
-            ])
+            sg.VPush(),
+            sg.FolderBrowse('..', font='arial 8 bold'),
+            sg.Input('PLACEHOLDER', key='file directory', size=(35,1), font='arial 11', enable_events=True)
+        ],
+
+        [
+            sg.VPush(),
+            sg.FolderBrowse('..', font='arial 8 bold'),
+            sg.Input('PLACEHOLDER', key='analysis dump directory', size=(35,1), font='arial 11', enable_events=True)
+        ], 
+        [sg.VPush()],
+        [
+            sg.VPush(),
+            sg.Button('START ANALYSIS', font='verdana 15 bold', button_color='black on ', size=(20,3), key='start')
         ]
-    ]
+    ], size=frames_size, relief=sg.RELIEF_SUNKEN, pad=(0, 0))]]
+
+    output_frame = [[sg.Frame('Output', [
+
+        [sg.Multiline(
+            size=(40, 15), 
+            key='OUTPUT', 
+            autoscroll=True, 
+            disabled=True, 
+            background_color='black', 
+            text_color='white')]
+    ], size=frames_size, pad=(0, 0))]]
 
 
-    layout = [
-        [sg.Column(input_frame)]
-        
-    ]
+    layout = [[
+        sg.Column(input_frame, vertical_alignment = 't'),
+        sg.Column(output_frame, pad=(0, 0))
+    ]]
 
 
 
@@ -48,8 +69,10 @@ def main():
         print(event, values)
         if event == sg.WIN_CLOSED:
             break
-        if event == 'Show':
-            window['-OUTPUT-'].update(values['solc'])
+        if event == 'start':
+            window['OUTPUT'].update(
+                f'{values["OUTPUT"]}\nsla oq coimeço aqui este arquivo da silva: {values["sol file"]}'
+            )
 
     window.close()
 
