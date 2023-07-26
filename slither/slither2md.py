4ca0d959575790fb1ae4a0d62bc08d1419017dfe
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2023-07-26 16:19:50 -0300

    Add files via upload

--- slither/slither2md.py
+++ slither/slither2md.py
@@ -1,7 +1,10 @@
 from os import popen
+from os import getcwd
 
 
 
 solfile = input('sol file (without .sol): ')
-analysis = popen(f'slither {solfile}.sol --checklist --show-ignored-findings').read()
+current_path = f'../{getcwd().split("/")[-1]}/{solfile}' # becomes ../current_dir/file
+
+analysis = popen(f'slither {current_path}.sol --checklist --show-ignored-findings').read()
 open(f'{solfile}_analyzed.md', 'w').write(analysis)
